758e7afddfd7d3a051faf2ebea794f2351739ce8
shippingport <61030366+shippingport@users.noreply.github.com>
2022-08-12 15:36:07 +0200

    Update AutopilotAssistant.ps1

--- Assistant/AutopilotAssistant.ps1
+++ Assistant/AutopilotAssistant.ps1
@@ -96,23 +96,23 @@ $PSThread_UI = [powershell]::Create().AddScript({
 
         <!--Network indicator-->
         <Ellipse Name="StatusIndicator_NetConnectionAvailable"  HorizontalAlignment="Left" Height="15" Width="15" Margin="-3,32,0,0" Fill="Red" VerticalAlignment="Top"/>
-        <Label Content="Network connection" HorizontalAlignment="Left" Margin="17,27,0,0" VerticalAlignment="Top" Width="212"/>
+        <Label Name="Label_NetworkStatus" Content="Checking internet connectivity..." HorizontalAlignment="Left" Margin="17,27,0,0" VerticalAlignment="Top" Width="212"/>
 
         <!--Intune servers pingable-->
         <Ellipse Name="StatusIndicator_IntunePingResponse"  HorizontalAlignment="Left" Height="15" Width="15" Margin="-3,58,0,0" Fill="Red" VerticalAlignment="Top"/>
-        <Label Content="Intune is pingable" HorizontalAlignment="Left" Margin="17,51,0,0" VerticalAlignment="Top" Width="212"/>
+        <Label Name="Label_IntunePingable" Content="Pinging Intune servers.." HorizontalAlignment="Left" Margin="17,51,0,0" VerticalAlignment="Top" Width="212"/>
 
         <!-- Certreq's AIK vertificate request result-->
-        <Ellipse Name="StatusIndicator_AIKCertRequestStatus" ToolTipService.ToolTip="Red = certificate was not issued. Consider clearing the TPM."  HorizontalAlignment="Left" Height="15" Width="15" Margin="-3,82,0,0" Fill="Red" VerticalAlignment="Top"/>
-        <Label Content="AIK certificate status" HorizontalAlignment="Left" Margin="17,77,0,0" VerticalAlignment="Top" Width="212"/>
+        <Ellipse Name="StatusIndicator_AIKCertRequestStatus" ToolTipService.ToolTip="Red = certificate was not issued. Consider clearing the TPM."  HorizontalAlignment="Left" Height="15" Width="15" Margin="-3,82,0,0" Fill="Orange" VerticalAlignment="Top"/>
+        <Label Content="AIK certificate status" Name="Label_CertReqLabel" HorizontalAlignment="Left" Margin="17,77,0,0" VerticalAlignment="Top" Width="212"/>
 
         <!-- AzureADJoined status -->
         <Ellipse Name="StatusIndicator_AzureADJoinedIndicator" ToolTipService.ToolTip="" HorizontalAlignment="Left" Height="15" Width="15" Margin="-3,108,0,0" Fill="Orange" VerticalAlignment="Top"/>
         <Label Content="Azure AD joined status" Name="Label_AzureADJoinStatus" HorizontalAlignment="Left" Margin="17,103,0,0" VerticalAlignment="Top" Width="212"/>
     
         <!--Shutdown and reboot buttons-->
-        <Button Content="Reboot" Name="Button_Reboot" HorizontalAlignment="Left" Height="32" Width="115" Margin="0,114,0,0" Grid.Row="1" VerticalAlignment="Top"/>
-        <Button Content="Shut down" Name="Button_Shutdown" HorizontalAlignment="Left" Height="32" Width="115" Margin="123,114,0,0" Grid.Row="1" VerticalAlignment="Top"/>
+        <Button Content="Reboot" Name="Button_Reboot" HorizontalAlignment="Left" Height="32" Width="106" Margin="0,104,0,0" Grid.Row="1" VerticalAlignment="Top"/>
+        <Button Content="Shut down" Name="Button_Shutdown" HorizontalAlignment="Left" Height="32" Width="106" Margin="115,104,0,0" Grid.Row="1" VerticalAlignment="Top"/>
 
     </Grid>
     
@@ -147,17 +147,25 @@ $PSThread_TrafficLightChecks = [powershell]::Create().AddScript({
     $NetworkConnection = Get-NetRoute | Where-Object DestinationPrefix -eq '0.0.0.0/0' | Get-NetIPInterface | Where-Object ConnectionState -eq 'Connected'
     if($NetworkConnection -ne $null) {
         $SyncHash.StatusIndicator_NetConnectionAvailable.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_NetConnectionAvailable.Fill = "Green"},9) # network is up
+        $SyncHash.Label_NetworkStatus.Dispatcher.Invoke([action]{$SyncHash.Label_NetworkStatus.Content = "Internet is connected."},9)
+
     } else {
         $SyncHash.StatusIndicator_NetConnectionAvailable.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_NetConnectionAvailable.Fill = "Red"},9) # network is down
+        $SyncHash.Label_NetworkStatus.Dispatcher.Invoke([action]{$SyncHash.Label_NetworkStatus.Content = "Can't connect to the internet"},9)
+
     }
 
     # Check if Intune is pingable
     if(Test-Connection -ComputerName endpoint.microsoft.com -Count 1)
     {
         $SyncHash.StatusIndicator_IntunePingResponse.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_IntunePingResponse.Fill = "Green"},9)
+        $SyncHash.Label_IntunePingable.Dispatcher.Invoke([action]{$SyncHash.Label_IntunePingable.Content = "Intune is pingable"},9)
+
     }
     else {
         $SyncHash.StatusIndicator_IntunePingResponse.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_IntunePingResponse.Fill = "Red"},9)
+        $SyncHash.Label_IntunePingable.Dispatcher.Invoke([action]{$SyncHash.Label_IntunePingable.Content = "Intune was not pingable."},9)
+
     }
 
     $dsregresult = (dsregcmd /status)
@@ -186,19 +194,23 @@ $PSThread_TrafficLightChecks = [powershell]::Create().AddScript({
     {
         $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.Fill = "Green"},9)
         $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.ToolTip = "Green = The certificate was issued. TPM attestation succeeded."},9)
+        $SyncHash.Label_CertReqLabel.Dispatcher.Invoke([action]{$SyncHash.Label_CertReqLabel.Content = "TPM passed attestation"},9)
     }
     elseif (($LastExitCode -eq "-2145844848")) {
-        $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.Fill = "Orange"},9) # HTTP_BADREQUEST"
+        $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.Fill = "Red"},9) # HTTP_BADREQUEST"
         $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.ToolTip = "Orange = certreq returned HTTP_BADREQUEST."},9)
+        $SyncHash.Label_CertReqLabel.Dispatcher.Invoke([action]{$SyncHash.Label_CertReqLabel.Content = "Certreq returned HTTP_BADREQUEST"},9)
+    }
+    elseif (($LastExitCode -eq "-2147024809")) {
+        $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.Fill = "Red"},9) # ACCESS_DENIED"
+        $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.ToolTip = "Orange = certreq returned ERROR_INVALID_PARAMETER."},9)
+        $SyncHash.Label_CertReqLabel.Dispatcher.Invoke([action]{$SyncHash.Label_CertReqLabel.Content = "Certreq returned ERROR_INVALID_PARAMETER"},9)
     }
     else {
         $SyncHash.StatusIndicator_AIKCertRequestStatus.Dispatcher.Invoke([action]{$SyncHash.StatusIndicator_AIKCertRequestStatus.Fill = "Red"},9) # Any other error condition
+        $SyncHash.Label_CertReqLabel.Dispatcher.Invoke([action]{$SyncHash.Label_CertReqLabel.Content = "TPM did not pass attestation!"},9)
         # Don't need to update the tooltip here, as red is the default state and is defined in the xaml.
     }
-
-
-
-
     #endregion checks
 
     $SyncHash.MainStatusMessage.Dispatcher.Invoke([action]{$SyncHash.MainStatusMessage.Content = "Ready."},9)
